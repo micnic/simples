572faf1e87bd61889096a89d1c9ddb2e351d5144
micnic <micnic@micnic-Aspire-7730.(none)>
2012-11-03 23:22:26 +0200

    0.0.3: removed a ws bug

--- index.js
+++ index.js
@@ -16,35 +16,35 @@ function simples(port) {
 	server.listen(port);
 	var started = true;
 
-	// Route all king of requests
+	// Route all kind of requests
 	this.all = function (path, callback) {
 		server.ALLroutes[url.parse(path).pathname] = callback;
 		return this;
-	}
+	};
 
 	// Route get requests
 	this.get = function (path, callback) {
 		server.GETroutes[url.parse(path).pathname] = callback;
 		return this;
-	}
-
-	// Route not found requests
-	this.notFound = function (callback) {
-		server.notFound = callback;
-		return this;
-	}
-
-	// Route post requests
-	this.post = function (path, callback) {
-		server.POSTroutes[url.parse(path).pathname] = callback;
-		return this;
-	}
+	};
 
 	// Route static files from specific local path
 	this.getStatic = function (path) {
 		server.staticRoute = path;
 		return this;
-	}
+	};
+
+	// Route not found requests
+	this.notFound = function (callback) {
+		server.notFound = callback;
+		return this;
+	};
+
+	// Route post requests
+	this.post = function (path, callback) {
+		server.POSTroutes[url.parse(path).pathname] = callback;
+		return this;
+	};
 
 	// Start simples server
 	this.start = function (port, callback) {
@@ -53,16 +53,18 @@ function simples(port) {
 				server.listen(port, callback);
 			});
 		} else {
+			started = true;
 			server.listen(port, callback);
 		}
 		return this;
-	}
+	};
 
 	// Stop simples server
 	this.stop = function (callback) {
+		started = false;
 		server.close(callback);
 		return this;
-	}
+	};
 
 	// New WebSocket host
 	this.ws = function (url, config, callback) {
@@ -72,29 +74,21 @@ function simples(port) {
 			server.on('upgrade', function (request, socket, head) {
 
 				// Handle for WebSocket requests
-				ws(request, socket, wsHosts[request.url]);	
+				ws(request, socket, server.wsHosts[request.url]);	
 			});
 		}
 
-		// Default configuration for websocket hosts
+		// Loading configuration for the WebSocket host
 		server.wsHosts[url] = {
 			config: {
-				messageMaxLength: 1048575,
-				origins: [''],
-				protocols: ['']
+				messageMaxLength: config.messageMaxLength || 1048575,
+				origins: config.origins || [''],
+				protocols: config.protocols || ['']
 			},
 			connections: [],
 			callback: callback
 		};
 
-		// Change configuration
-		var _cacheHasOwnProperty = Object.prototype.hasOwnProperty;
-		for (var i in config) {
-			if (_cacheHasOwnProperty.call(config, i) && server.wsHosts[url].config[i]) {
-				server.wsHosts[url].config[i] = config[i];
-			}
-		}
-
 		return this;
-	}
+	};
 }
